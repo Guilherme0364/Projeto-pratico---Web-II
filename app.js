50d6d0801b0c21e9461ad518e2bf9631fde9df1a
android <android@LABFAE.local>
2024-08-20 20:53:49 -0300

    final da primeira aula

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const express = require("express"); 
 const app = express(); 
 
+// Renderizador de HTML
 app.set("view engine", "ejs")
- 
-app.get("/", function (req, res) { 
-   res.render(('../view/index'))
-}); 
+
+app.get("/:nome/:dev", (req,res) => {
+    var nome = req.params.nome
+    var dev = req.params.dev
+    var exibirMsg = true
+
+    var produtos = [
+        {
+            "nome": "MacBook Pro",
+            "preco": "R$ 2.000,00",
+            "quantidade": 1
+        },
+        {
+            "nome": "iPhone 11",
+            "preco": "R$ 1.000,00",
+            "quantidade": 1
+        },
+        {
+            "nome": "iPhone 11 Pro",
+            "preco": "R$ 1.500,00",
+            "quantidade": 1
+        }
+    ]
+
+    res.render(('../view/index'),{
+        nome: nome,
+        dev: dev,
+        company: "Unifae",
+        salary: "R$ 1.000.000,00",
+        msg: exibirMsg,
+        produtos: produtos
+    })
+})
  
 app.listen(8181, function (erro) { 
    if (erro) { 
