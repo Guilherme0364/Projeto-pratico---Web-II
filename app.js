fbfdca0987bbad1751cb100ca22d45c69c7cfb35
android <android@LABFAE.local>
2024-08-20 22:37:50 -0300

    commit do final da ultima aula

--- app.js
+++ app.js
@@ -4,39 +4,16 @@ const app = express();
 // Renderizador de HTML
 app.set("view engine", "ejs")
 
-app.get("/:nome/:dev", (req,res) => {
-    var nome = req.params.nome
-    var dev = req.params.dev
-    var exibirMsg = true
+app.use(express.static("public"));
 
-    var produtos = [
-        {
-            "nome": "MacBook Pro",
-            "preco": "R$ 2.000,00",
-            "quantidade": 1
-        },
-        {
-            "nome": "iPhone 11",
-            "preco": "R$ 1.000,00",
-            "quantidade": 1
-        },
-        {
-            "nome": "iPhone 11 Pro",
-            "preco": "R$ 1.500,00",
-            "quantidade": 1
-        }
-    ]
-
-    res.render(('../view/index'),{
-        nome: nome,
-        dev: dev,
-        company: "Unifae",
-        salary: "R$ 1.000.000,00",
-        msg: exibirMsg,
-        produtos: produtos
-    })
+app.post("/savequestion", (req,res) => {
+    res.send("Formulário recebifo com sucesso!")
 })
  
+app.get("/question", (req,res) => {
+    res.render(('../view/question'))
+})
+
 app.listen(8181, function (erro) { 
    if (erro) { 
        console.log("Erro"); 
